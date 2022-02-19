7be9f452b76b7683634c1c91d03c4a1b9aaeaa14
seungwon-yoo <qwer15417@gmail.com>
2022-02-19 11:09:32 +0900

    Initial Commit

---
+++ ClothSimulation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClothSimulation
+//
+//  Created by 유승원 on 2022/02/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
