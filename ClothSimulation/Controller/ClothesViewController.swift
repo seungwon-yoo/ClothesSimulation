d8270a61471287cc75e427b4a556c12d229a51af
seungwon-yoo <qwer15417@gmail.com>
2022-03-09 00:01:13 +0900

    add drag from side to side.

--- ClothSimulation/Controller/ClothesViewController.swift
+++ ClothSimulation/Controller/ClothesViewController.swift
@@ -11,6 +11,7 @@ import SceneKit
 
 class ClothesViewController: UIViewController {
     
+    @IBOutlet var pinchGestureRecognizer: UIPinchGestureRecognizer!
     @IBOutlet weak var toolbar: UIToolbar!
     @IBOutlet weak var sceneView: SCNView!
     @IBOutlet weak var collectionView: UICollectionView!
@@ -20,13 +21,39 @@ class ClothesViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         toolbar.items![toolbar.items!.startIndex].tintColor = .systemBlue
-        set3DModel(name: "art.scnassets/male.obj")
+        set3DModel(name: "art.scnassets/FinalBaseMesh.obj")
         startInitialSettings()
+        print(sceneView.transform)
+    }
+    
+    @IBAction func panAction(_ sender: UIPanGestureRecognizer) {
+        let transition = sender.translation(in: sceneView)
+        sceneView.defaultCameraController.rotateBy(x: Float(-transition.x), y: Float(0))
+        sender.setTranslation(CGPoint.zero, in: sceneView)
+    }
+    
+    @IBAction func pinchAction(_ sender: UIPinchGestureRecognizer) {
+        // 모델 자체를 확대 축소해야 한다.
+        if sender.state == .began || sender.state == .changed {
+            // 스택오버플로 참고하고 있었음. rootNode말고 하나하나 해볼 것.
+            let node = sceneView.scene?.rootNode.transform
+            let translation = SCNMatrix4MakeTranslation(1.0, 1.0, Float(sender.scale))
+            let newTrans = SCNMatrix4Mult(node!, translation)
+            sceneView.scene?.rootNode.transform = newTrans
+            sender.scale = 1.0
+            
+//            sender.view?.transform = (sender.view?.transform.scaledBy(x: sender.scale, y: sender.scale))!
+//            sender.scale = 1.0
+       }
     }
     
     @IBAction func changeButtonPressed(_ sender: UIButton) {
         // Create the action buttons for the alert.
-        let manAction = UIAlertAction(title: "남성",
+        let bulkyManAction = UIAlertAction(title: "다부진 남성",
+                                      style: .default) { (action) in
+            self.set3DModel(name: "art.scnassets/FinalBaseMesh.obj")
+        }
+        let skinnyManAction = UIAlertAction(title: "마른 남성",
                                       style: .default) { (action) in
             self.set3DModel(name: "art.scnassets/male.obj")
         }
@@ -40,7 +67,9 @@ class ClothesViewController: UIViewController {
         let alert = UIAlertController(title: "모델 선택",
                                       message: "",
                                       preferredStyle: .actionSheet)
-        alert.addAction(manAction)
+        
+        alert.addAction(bulkyManAction)
+        alert.addAction(skinnyManAction)
         alert.addAction(womanAction)
         alert.addAction(cancelAction)
         
@@ -64,7 +93,6 @@ class ClothesViewController: UIViewController {
         
         self.collectionView.reloadData()
     }
-    
 }
 
 extension ClothesViewController {
@@ -107,36 +135,46 @@ extension ClothesViewController {
         cameraNode.camera = SCNCamera()
         
         // 3: Place camera
-        cameraNode.position = SCNVector3(x: 0, y: 40, z: 100)
+        cameraNode.position = SCNVector3(x: 0, y: 10, z: 25)
         
         // 4: Set camera on scene
         scene?.rootNode.addChildNode(cameraNode)
         
-        // 5: Adding light to scene
+        // 5-1: Adding front light to scene
         let lightNode = SCNNode()
         lightNode.light = SCNLight()
         lightNode.light?.type = .omni
-        lightNode.position = SCNVector3(x: 0, y: 40, z: 100)
+        lightNode.position = SCNVector3(x: 0, y: 10, z: 25)
         scene?.rootNode.addChildNode(lightNode)
+        
+        // 5-2: Adding back light to scene
+        let backLightNode = SCNNode()
+        backLightNode.light = SCNLight()
+        backLightNode.light?.type = .omni
+        backLightNode.position = SCNVector3(x: 0, y: 10, z: -25)
+        scene?.rootNode.addChildNode(backLightNode)
         
         // 6: Creating and adding ambient light to scene
         let ambientLightNode = SCNNode()
         ambientLightNode.light = SCNLight()
         ambientLightNode.light?.type = .ambient
-        ambientLightNode.light?.color = UIColor.darkGray
+        ambientLightNode.light?.color = UIColor.lightGray
         scene?.rootNode.addChildNode(ambientLightNode)
         
         // If you don't want to fix manually the lights
         // sceneView.autoenablesDefaultLighting = true
         
         // Allow user to manipulate camera
-        sceneView.allowsCameraControl = true
+        // sceneView.allowsCameraControl = true
+        
+        // Set to manipulate camera
+        
         
         // Show FPS logs and timming
         // sceneView.showsStatistics = true
         
         // Set background color
-        sceneView.backgroundColor = UIColor.white
+        sceneView.backgroundColor = UIColor.systemGray
         
         // Allow user translate image
         sceneView.cameraControlConfiguration.allowsTranslation = false
